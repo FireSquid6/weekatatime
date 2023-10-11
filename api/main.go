f6b1fa242507aeedfb0914b22cb57f299982dc6b
firesquid <jdeiss06@gmail.com>
2023-10-10 21:02:43 -0500

    use echo instead of gin

--- api/main.go
+++ api/main.go
@@ -2,7 +2,10 @@ package main
 
 import (
 	"fmt"
-	"github.com/gin-gonic/gin"
+	"github.com/joho/godotenv"
+	"github.com/labstack/echo/v4"
+	"net/http"
+	"os"
 )
 
 type task struct {
@@ -13,13 +16,20 @@ type task struct {
 }
 
 func main() {
-	r := gin.Default()
-	r.GET("/ping", func(c *gin.Context) {
-		c.JSON(200, gin.H{
-			"message": "pong",
-		})
-	})
+	err := godotenv.Load()
+	if err != nil {
+		fmt.Println("Error loading .env file")
+	}
 
-	fmt.Println("Running gin...")
-	r.Run(":8080")
+	var port = os.Getenv("PORT")
+	fmt.Println("Port: " + port)
+	server(port)
+}
+
+func server(port string) {
+	e := echo.New()
+	e.GET("/", func(c echo.Context) error {
+		return c.String(http.StatusOK, "Hello, World!")
+	})
+	e.Logger.Fatal(e.Start(":" + port))
 }
