d182c199af9e47b000c53787c0d2896adefbf3af
firesquid <jdeiss06@gmail.com>
2023-10-10 20:34:01 -0500

    add basic http server

--- api/main.go
+++ api/main.go
@@ -2,8 +2,24 @@ package main
 
 import (
 	"fmt"
+	"github.com/gin-gonic/gin"
 )
 
+type task struct {
+	ID          int    `json:"id"`
+	Name        string `json:"name"`
+	Description string `json:"description"`
+	Completed   bool   `json:"completed"`
+}
+
 func main() {
-	fmt.Println("hello world!")
+	r := gin.Default()
+	r.GET("/ping", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "pong",
+		})
+	})
+
+	fmt.Println("Running gin...")
+	r.Run(":8080")
 }
